77867ea9445564364d045953c14ee4097e55bb16
Aaron Guo <aaronguostudio@gmail.com>
2019-07-01 16:02:22 -0600

    add wx auth manual

---
+++ src/main/java/com/imooc/sell/controller/WeixinController.java
@@ -0,0 +1,27 @@
+package com.imooc.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+@RequestMapping("/weixin")
+public class WeixinController {
+    @GetMapping("/auth")
+    public void auth (
+        @RequestParam("code") String code
+    ) {
+        log.info("code is {}", code );
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=appid&secret=secret&code=" + code + "&grant_type=authorization_code";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String res = restTemplate.getForObject(url, String.class);
+
+        log.info("res={}", res);
+    }
+}
